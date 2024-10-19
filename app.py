a841d4518addb514ec60297af568146b57b3ef3b
ali-ali18 <alisson.camargo024@gmail.com>
2024-10-19 16:24:51 -0300

    add cors para portas diferentes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from json_utils import inicializa_json
 from routes import registrar_rotas
 #Inicia o flask
 app = Flask(__name__)
+CORS(app)
 
 #incia a função verificação do json existe
 inicializa_json()
